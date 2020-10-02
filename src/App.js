86311e6d2022da62240eefa4a5380721ea3b30a6
Luis García <aig.luis@gmail.com>
2020-10-02 13:16:54 -0500

    proyecto iniciado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  componentDidMount() {
+    this.fetchPokemon();
+  }
+
+  fetchPokemon = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+
+    const data = await response.json();
+
+    this.setState({
+      data: data,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ul></ul>
+      </div>
+    );
+  }
 }
 
 export default App;
